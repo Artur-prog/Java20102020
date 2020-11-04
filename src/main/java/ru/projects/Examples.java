43a65b4a1c68b5458abcc0b2bc5e0669993b25fd
Artur-prog <AGaribyan-1@yandex.ru>
2020-11-04 14:49:12 +0300

    Init2

--- src/main/java/ru/projects/Examples.java
+++ src/main/java/ru/projects/Examples.java
@@ -10,14 +10,28 @@ public class Examples {
         return (byte) (x + y);
     }
 
+     /* 2. Написать публичный метод принимающий 2 параметра: первый типа int и второй типа long.
+       Метод должен возвращать произведение значений параметров. Тип возвращаемого значения int. */
+
+    public static int multiplicationByteAndLong(int i, long l) {
+        return (int) (i*l);
+    }
+
 
 
     public static void main(String[] args) {
         byte x = 63;
         short y = 53;
+        int i = 50;
+        long l= 616649797L;
 
 
         // 1.
         System.out.println(sumByteAndShort(x,y));
+
+        // 2.
+        System.out.println(multiplicationByteAndLong(i,l));
+
+
     }
 }
