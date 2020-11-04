414e917af0279cdab770ce083b923594cdc399ae
Artur-prog <AGaribyan-1@yandex.ru>
2020-11-04 14:53:35 +0300

    Branch1 - commit2

--- src/main/java/ru/projects/Examples.java
+++ src/main/java/ru/projects/Examples.java
@@ -17,6 +17,69 @@ public class Examples {
         return (int) (i*l);
     }
 
+    /* 3. Написать метод getMaxNumber принимающий 2 параметра: int и int.
+       Метод должен сравнить переданные параметры между собой и вернуть максимальный.
+       Для решения необходимо использовать IF или тернарный оператор ?: */
+
+    public static int getMaxNumber  (int a ,int b){
+        if(a > b){
+            return a;
+        } else if(b > a){
+            return b;
+        }else{
+            return a;
+        }
+
+    }
+
+    /* 4. Написать метод isCharA принимающий 1 параметр типа char. Если передана буква 'А',
+       то возвращать true, а в остальных случаях false */
+
+    public static boolean isCharA  (char c){
+        return  c == 'A';
+    }
+
+    /* 5. Написать метод isCharNumber принимающий 1 параметр типа char. Если передано число, то вернуть true,
+       а в остальных случаях false */
+
+    public static boolean isCharNumber  (char c){
+        return Character.isDigit(c);
+    }
+
+    /* 6. Написать метод выводящий на экран в цикле бинарное представление чисел от 0 до 30.
+       Для вывода бинарного представления использовать Integer.toBinaryString().
+       Без входных параметров и не возвращающая ничего. */
+
+    public static void printToBinaryString() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println(Integer.toBinaryString(i));
+        }
+    }
+
+    /* 7. Написать метод выводящий бинарное представление Integer.MAX_VALUE и Integer.MAX_VALUE + 1.
+       Для вывода бинарного представления использовать Integer.toBinaryString().
+       Без входных параметров и не возвращающая ничего.
+       Покажет, что переполнение не вызывает ошибку и как это выглядет в памяти */
+
+    public static void intMaxValue(){
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE +1));
+    }
+
+    /* 8. Написать метод принимающий 1 параметр типа char и возвращающий его числовое представление. */
+
+    public static int charToInt (char c){
+        return (int) c;
+    }
+
+    /* 9. Написать метод принимающий 1 параметр типа int и возвращающий его представление в виде
+       печатного символа */
+
+    public static char intToChar  (int i){
+        return (char) i;
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -31,6 +94,29 @@ public class Examples {
 
         // 2.
         System.out.println(multiplicationByteAndLong(i,l));
+
+        // 3.
+        System.out.println(getMaxNumber(350,200));
+
+        // 4.
+        System.out.println(isCharA('A'));
+        System.out.println(isCharA('B'));
+
+        // 5.
+        System.out.println(isCharNumber( '1' ));
+        System.out.println(isCharNumber( 'A' ));
+
+        // 6.
+        printToBinaryString();
+
+        // 7.
+        intMaxValue();
+
+        // 8.
+        System.out.println( charToInt( 'H' ));
+
+        // 9.
+        System.out.println(intToChar(31));
 
 
     }
